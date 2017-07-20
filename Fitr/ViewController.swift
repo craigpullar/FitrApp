7f6634b157e90452854a9bf156696c4de068bb48
craigpullar <craigmichaelpullar@gmail.com>
2017-07-20 05:17:06 +0100

    Initial Commit

---
+++ Fitr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fitr
+//
+//  Created by Craig Pullar on 7/20/17.
+//  Copyright © 2017 Craig PullarFitr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
